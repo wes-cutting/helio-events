5373f4463eaea67d22886ce2d789a0a539e5b33d
Corey Bernsdorff <coreycbernsdorff@gmail.com>
2018-08-17 14:54:34 -0600

    updated schema and database

--- client/src/components/courses/CourseDetails.js
+++ client/src/components/courses/CourseDetails.js
@@ -2,3 +2,5 @@
 // Shows Calendar of Course
 // Settings button here that navigates to CourseSettings.js
 // Delete button here that pops up a Delete Modal
+//
+// Corey is making a Figma for this functionality
