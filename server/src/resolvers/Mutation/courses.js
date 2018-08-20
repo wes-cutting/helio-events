28d04d5e908716ec51845e7b0968218480c41afd
Nathanael Sherwood <Sherwood.Nathanael@gmail.com>
2018-08-20 16:16:28 -0600

    resolvers for create course done

--- server/src/resolvers/Mutation/courses.js
+++ server/src/resolvers/Mutation/courses.js
@@ -2,8 +2,62 @@ const { getUserId } = require('../../utils')
 // createCourse(template: ID!, name: String!, start: DateTime!): Course!
 // updateCourse(template: ID!, name: String!, start: DateTime!, start: DateTime!, events: [ID!]!, isFinished: Boolean!): Course!
 // deleteCourse(id: ID!): Course!
-const courseTemplates = {
 
-}
+const courses = {
+    async createCourse(parent, {name, start, isFinished, template}, ctx, info) {
+        return ctx.db.mutation.createCourse(
+            {
+                data: {
+                    name,
+                    start,
+                    isFinished: false,
+                    template: {
+                        connect: { id: template},
+                    }
+                    },
+                },
+            info
+            );
+        },
 
-module.exports = { courseTemplates }
+    async updateCourse(parent, { id, name, start, isFinished, events }, ctx, info) {
+        const courseExists = await ctx.db.exists.Course({
+            id,
+        })
+
+        if (!courseExists) {
+            throw new Error(`Course not found`)
+        }
+
+        return ctx.db.mutation.updateCourse(
+            {
+                where: {id},
+                data: {
+                    name,
+                    start,
+                    isFinished,
+                    events: {
+                        set: {events}
+                    }
+                },
+            },
+            info,
+        );
+    },
+
+    async deleteCourse(parent, { id }, ctx) {
+        const courseExists = await ctx.db.exists.Course({
+            id,
+        })
+
+        if (!courseExists) {
+            throw new Error(`Course not found`)
+        }
+
+        return ctx.db.mutation.deleteCourse({ where: { id } })
+    },
+};
+
+
+
+module.exports = { courses }
