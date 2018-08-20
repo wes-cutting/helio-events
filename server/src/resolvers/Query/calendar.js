021b74049fe025294a1ca97a551da6422f4a08b1
Corey Bernsdorff <coreycbernsdorff@gmail.com>
2018-08-20 17:52:33 -0600

    working on query resolvers

--- server/src/resolvers/Query/calendar.js
+++ server/src/resolvers/Query/calendar.js
@@ -8,9 +8,34 @@ const { getUserId } = require('../../utils')
 // courses: [Course!]!
 
 const calendar = {
-    event(parent, { id }, ctx, info) {
-        return ctx.db.query.event({ where: { id } }, info)
-    },
+   // list of EVENTS nav'd from Main Menu
+   events(parent, { id, name, desc, date }, ctx, info) {
+      return ctx.db.query.events({ where: { id } }, info)
+   },
+   // list of TEMPLATES nav'd from main menu
+   courseTemplates(parent, { id, name, courseKind, campus, days, hours }, ctx, info) {
+      return ctx.db.query.courseTemplates({ where: { id } }, info)
+   },
+   // list of COURSES nav'd from main menu
+   courses(parent, { id }, ctx, info) {
+      return ctx.db.query.courses({ where: { id } }, info)
+   },
+   
+   eventByKind(parent, { eventKind }, ctx, info) {
+      return ctx.db.query.eventByKind({ where: { EventKind: eventKind } }, info)
+   },
+   
+   course(parent, { id }, ctx, info) {
+      return ctx.db.query.course({ where: { id } }, info)
+   },
+   
+   event(parent, { id }, ctx, info) {
+      return ctx.db.query.event({where: { id }}, info)
+   },
+   
+   courseTemplate(parent, { id }, ctx, info) {
+      return ctx.db.query.courseTemplate({ where: { id } }, info)
+   },
 }
 
 module.exports = { calendar }
